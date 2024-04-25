8051303de125acbce11bcfc39512ff47ac8fd0f3
digitaloutbreak <joeynalvarado@gmail.com>
2024-04-25 15:21:55 -0500

    add login signup pricing

--- app/page.tsx
+++ app/page.tsx
@@ -8,28 +8,41 @@ import { GithubIcon } from '@/components/icons'
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <h1 className={title()}>The&nbsp;</h1>
-        <h1 className={title({ color: 'violet' })}>All-in-One&nbsp;</h1>
+    <section className='flex flex-col items-center justify-center gap-6 py-8 md:py-10'>
+      <div className='inline-block max-w-xl text-center justify-center'>
+        <h1 className={title({ size: 'lg' })}>The&nbsp;</h1>
+        <h1 className={title({ color: 'violet', size: 'lg' })}>
+          All-in-One&nbsp;
+        </h1>
         <br />
-        <h1 className={title()}>Client Management Suite</h1>
-        <h2 className={subtitle({ class: 'mt-5' })}>
+        <h1 className={title({ size: 'lg' })}>Client Management Suite</h1>
+        <h2 className={subtitle({ class: 'mt-5 text-5xl' })}>
           Manage and collaborate with clients using one integrated platform
         </h2>
       </div>
 
-      <div className="flex gap-3">
+      <div className='flex gap-6'>
         <Link
           isExternal
-          href={siteConfig.links.dashboard}
+          href='/login'
           className={buttonStyles({
             color: 'primary',
-            radius: 'full',
+            // radius: 'full',
             variant: 'shadow',
+            size: 'lg',
           })}
         >
-          Start Here
+          Get Started
+        </Link>
+        <Link
+          href='/signup'
+          className={buttonStyles({
+            variant: 'shadow',
+            color: 'primary',
+            size: 'lg',
+          })}
+        >
+          Login
         </Link>
         {/* <Link
           isExternal
